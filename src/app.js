b8a023a6c43d2cc47808c95fd080390c5ed8ea52
Chengsong <shoumatu1996@gmail.com>
2016-09-17 15:18:04 +0900

    renamed users to feels

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export class App {
     config.title = 'Stress Catcher';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: './users',        nav: true, title: 'Github Users' },
+      { route: 'feels',         name: 'feels',        moduleId: './feels',        nav: true, title: 'User Feels' },
       { route: 'profile-router',  name: 'profile-router', moduleId: './profile-router', nav: true, title: 'My Account' }
     ]);
 
